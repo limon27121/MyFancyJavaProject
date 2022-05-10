3a0244cc5f9615d02f71390f728428e39706d298
Md.Moontaser Alam <moontaser009@gmail.com>
2022-05-11 00:36:33 +0600

    change varible name

--- pattern.java
+++ pattern.java
@@ -6,13 +6,13 @@ public static void main(String args[])
 {    
 Scanner sc= new Scanner(System.in);  
 System.out.print("Enter your desire number ");  
-int n= sc.nextInt(); 
+int n1= sc.nextInt(); 
 int row, col;       
   
-for (row=1; row<=n; row++)   
+for (row=0; row<n1; row++)   
 {
 
-    for (col = 1; col <= n - row; col++) {
+    for (col = 1; col <= n1 - row; col++) {
         System.out.print(" ");
     }
     for (col = 1; col <= 2 * row - 1; col++) {
